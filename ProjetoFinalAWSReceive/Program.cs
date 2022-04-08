7c43676172a39e65b694d5538a895a9234840b57
Caique Assis <caiqueassis.brazil@gmail.com>
2022-04-08 00:10:06 -0300

    Tempo para o canal não ser fechado e continuar sendo ouvinte

--- ProjetoFinalAWSReceive/Program.cs
+++ ProjetoFinalAWSReceive/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace ProjetoFinalAWSReceive
 {
@@ -10,25 +11,35 @@ namespace ProjetoFinalAWSReceive
         static void Main()
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
+            string nomeFila = "partida";
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "partida",
+                channel.QueueDeclare(queue: nomeFila,
                                      durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
 
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
                 {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine("Mensagem recebida: ", message);
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Mensagem recebida: {0}", message);
+                        channel.BasicAck(ea.DeliveryTag, false);
+                        Thread.Sleep(1000);
+
                 };
+
                 channel.BasicConsume(queue: "partida",
-                                     autoAck: true,
+                                     autoAck: false,
                                      consumer: consumer);
+
+                Console.WriteLine("Todas as mensagens lidas");
+                Console.ReadLine();
             }
         }
     }
